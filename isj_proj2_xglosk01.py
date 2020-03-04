a854ebf75a7a56dbdce5e6a96159718e84f4048b
Krystof Glos <stofaj@seznam.cz>
2020-03-04 16:40:09 +0100

    Final project,

--- isj_proj2_xglosk01.py
+++ isj_proj2_xglosk01.py
@@ -10,8 +10,8 @@ def she_says_he_says(she_says):
     'umiram'
     """
 
-    phonetic_she_says = she_says("y", "i")                  # vase reseni
-    compact = phonetic_she_says(" ", "")                              # vase reseni
+    phonetic_she_says = she_says.replace("y", "i")                  # vase reseni
+    compact = phonetic_she_says.replace(" ", "")                              # vase reseni
     he_says = compact[:: -1]                                        # vase reseni
     return he_says
 
@@ -31,22 +31,21 @@ def solfege(title_hymn):
     """
 
     # the input string partitioned to the title (if given) and the actual hymn
-    possible_title, sep, hymn = title_hymn, '',''                # vase reseni
+    possible_title, sep, hymn = title_hymn.rpartition(": ")                # vase reseni
 
     # the hymn as a list of strings separated by ' '
     hymn_list = hymn.split(" ")                                               # vase reseni
     
     # skipping always two strings, and ending 3 strings from the end
-    skip2 = hymn_list[0:3:-3]                                                   # vase reseni
+    skip2 = hymn_list[0:-3:3]                                                   # vase reseni
 
     # the skip2 list as a string, ', ' as a separator
-    skip2_str = ",".join(skip2)                                               # vase reseni
+    skip2_str = ", ".join(skip2)                                               # vase reseni
 
     return skip2_str
 
 
 if __name__ == "__main__":
-    #import doctest
-    #doctest.testmod()
-    pass
+    import doctest
+    doctest.testmod()
 
